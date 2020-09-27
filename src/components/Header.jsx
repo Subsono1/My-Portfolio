fb1addc5c2b2be9cefb9cdfbd457b78ad0b3fb60
Sebastian Duarte <Subsono33@gmail.com>
2020-09-27 18:06:27 -0400

    setting up the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="header-nav">
+     
+        <Link className="header-nav_links">
+          ABOUT 
+        </Link >
+        <Link className="header-nav_links">
+          PROJECTS 
+        </Link>
+        <Link className="header-nav_links">
+        CONTACT ME!
+        </Link>
+      </nav>
+      <div className="header-brand_div">
+        {/* <h1 className="header-brand">SD</h1> */}
+        <img src={ require('../assets/SD.png')} alt="logo-initials" className="header-brand">
+
+        </img>
+      
+      </div>
+      
+      <div className="header-text_box">
+        <h1 className="my-name">SEBASTIAN DUARTE</h1>
+        <h3 className="description">Full-stack Software Engineer</h3>
+      </div>
+    </header>
+   
+  )
+}
+
+export default Header
---
+++ src/screens/MainScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Switch } from 'react-router-dom'
+import Header from '../components/Header'
+
+function MainScreen() {
+  return (
+    <div>
+      <Header />
+  </div>
+  )
+}
+
+export default MainScreen
